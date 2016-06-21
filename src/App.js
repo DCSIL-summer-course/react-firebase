95dbb164edede9c1956889ce12ec48447c38dab2
Liam Kaufman <liam.kaufman@gmail.com>
2016-06-21 16:59:58 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+class App extends Component {
+  render(){
+    return (
+      <div>
+        <h1 style={{color: 'red'}}>
+          Hello World
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default App;
